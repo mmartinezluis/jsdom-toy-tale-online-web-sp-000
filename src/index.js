1d22fd95f76050f7ec2950a42c7307fe5cd48003
Luis Martinez <luis_mmartinez@live.com>
2021-06-07 08:06:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 let addToy = false;
+const collectionDiv = document.getElementById("toy-collection");
+const form = document.querySelector('form');
+// const likeButtons = document.querySelectorAll('.like-btn')
+let errorDiv = document.createElement('div')
+errorDiv.setAttribute('class', 'error');
+document.body.appendChild(errorDiv);
+
+function addToDom(toy) {
+  let newDiv = document.createElement('div');
+  newDiv.className= "card";
+  newDiv.innerHTML = 
+  `<h2>${toy.name}</h2>
+   <img sc="${toy.image}">
+   <p>${toy.likes} likes</p>
+   <button class="like-btn" id="${toy.id}">Like</button>`;
+  collectionDiv.appendChild(newDiv);
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  // const likeButtons = document.querySelectorAll('.like-btn');
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +30,91 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  return fetch("http://localhost:3000/toys")
+    .then( function(response) {
+      return response.json();
+    })
+    .then(function(toys) {
+      for (const toy of toys) {
+        addToDom(toy);
+      }
+      let likeButtons = document.querySelectorAll('.like-btn');
+      likeButtons.forEach( button => {
+        button.addEventListener('click', activateLikeButton())
+      })
+    });
 });
+
+function handleLikes() {
+  console.log("hello")
+  // return fetch(`http://localhost:3000/toys/${button.id}`, {
+  //   method: "PATCH",
+  //   headers: {
+  //     "Content-Type": "applicaotn/json",
+  //     "Accept": "application/json"
+  //   },
+  //   body: JSON.stringify({
+  //     likes: button.parentElement.querySelector('p').innerText++
+  //   })
+  // })
+  //   .then(function(response) {
+  //     return response.json();
+  //   })
+  //   .then(function(toy) {
+  //     increaseLike(toy);
+  //   })
+  //   .catch(function(error) {
+  //     errorDiv.innerHTML = error.message;
+  //   })
+}
+
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault()
+  let name = document.querySelectorAll('form input')[0].name;
+  let image = document.querySelectorAll('form input')[1].name
+  submitData(name, image);;
+});
+
+function submitData(toyName, imgUrl) {
+  return fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: toyName,
+      image: imgUrl,
+      likes: 0
+    })
+  })
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(toy) {
+      addToDom(toy);
+    })
+    .catch(function(error) {
+      document.body.innerHTML = error.message;
+    });
+}
+
+
+
+// likeButtons.forEach(button => {
+//   button.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     handleLikes();  
+//   })
+// })
+
+let activateLikeButton = function () {  
+  this.addEventListener('click', function(e) {
+    e.preventDefault();
+    handleLikes();  
+  })
+}
+
+
